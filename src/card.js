10a7fc0322519f0fad295cf567d0184ad8c7e4bf
Janne Vainio <janne.vainio@leadin.fi>
2023-12-26 16:22:17 +0000

    new card

---
+++ src/card.js
@@ -0,0 +1,40 @@
+class JgaugePanel extends HTMLElement {
+
+    config;
+    content;
+
+    // required
+    setConfig(config) {
+        this.config = config;
+    }
+
+    set hass(hass) {
+        const entityId = this.config.entity;
+        const state = hass.states[entityId];
+        const stateStr = state ? state.state : 'unavailable';
+
+        // done once
+        if (!this.content) {
+            // user makes sense here as every login gets it's own instance
+            this.innerHTML = `
+                <ha-card header="Hi ho ${hass.user.name}!">
+                    <div class="card-content"></div>
+                </ha-card>
+            `;
+            this.content = this.querySelector('div');
+        }
+        // done repeatedly
+        this.content.innerHTML = `
+            <p>The ${entityId} is ${stateStr}.</p>
+        `;
+    }
+}
+
+customElements.define('jgaugepanel-card', JgaugePanel);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+    type: "jgaugepanel-card",
+    name: "JgaugePanel Card",
+    description: "A custom card made by me!" // optional
+});
